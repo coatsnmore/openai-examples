58da0e41a3acee3989ddeb257f657480d61abe1a
Nick Coats <coats.18@gmail.com>
2023-02-02 14:19:36 -0500

    added functionality to save image from html; added copy to clipboard function;

--- web/index.js
+++ web/index.js
@@ -1,4 +1,48 @@
+function createBlob(base64Image) {
+    // Split the base64 string on the comma
+    var commaIndex = base64Image.indexOf(",") + 1;
+    var base64 = base64Image.substring(commaIndex);
 
+    // Decode the base64 string
+    var rawData = atob(base64);
+
+    // Create an ArrayBuffer with the binary data
+    var arrayBuffer = new ArrayBuffer(rawData.length);
+    var uint8Array = new Uint8Array(arrayBuffer);
+    for (var i = 0; i < rawData.length; i++) {
+        uint8Array[i] = rawData.charCodeAt(i);
+    }
+
+    // Create a Blob with the ArrayBuffer
+    var blob = new Blob([uint8Array], { type: "image/png" });
+    return blob;
+}
+
+
+function copyImage() {
+    html2canvas(document.getElementById("card")).then(function (canvas) {
+        let imgData = canvas.toDataURL("image/png");
+        let image = document.getElementById("shareable-image");
+        image.style.visibility = 'hidden';
+        image.src = imgData;
+        return image;
+    }).then(image => {
+        var blob = createBlob(image.src);
+        navigator.clipboard.write([
+            new ClipboardItem({
+                [blob.type]: blob,
+            }),
+        ])
+            .then(function () {
+                console.log("Image copied to the clipboard!");
+                document.getElementById("message").innerHTML = `Image copied to clipboard!`;
+            })
+            .catch(function (error) {
+                console.error("Could not copy image: ", error);
+                document.getElementById("message").innerHTML = `Could not copy image.`;
+            });
+    });
+}
 
 // async functions return promises and resolve with whatever is returned
 async function getCharacter() {
@@ -7,7 +51,7 @@ async function getCharacter() {
 
     const host = 'http://localhost:4000';
 
-    const types = ['dog', 'cat', 'bear', 'salamander', 'horse', 'seal', 'squirrel', 'giraffe', 'monkey', 'porcupine', 'mink', 'donkey', 'moose', 'gorilla', 'zebra', 'hyena', 'deer', 'elk', 'bird', 'lizard', 'snake', 'celestial', 'demon', 'angel', 'house', 'clown', 'hippo', 'planet', 'god', 'robot', 'mountain', 'forest', 'wolf', 'spider', 'mongoose', 'kangaroo', 'dinosaur'];
+    const types = ['dog', 'cat', 'bear', 'salamander', 'horse', 'seal', 'squirrel', 'giraffe', 'monkey', 'porcupine', 'mink', 'donkey', 'moose', 'gorilla', 'zebra', 'hyena', 'deer', 'elk', 'bird', 'lizard', 'snake', 'celestial', 'demon', 'angel', 'house', 'clown', 'hippo', 'planet', 'god', 'robot', 'forest', 'wolf', 'spider', 'mongoose', 'kangaroo', 'dinosaur'];
     const randomType = types[Math.floor(Math.random() * types.length)];
 
     // fetch returns promises
